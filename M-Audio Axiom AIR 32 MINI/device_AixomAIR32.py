0ab157ef729f0e2e5439d8f8f2455029043efa7f
giant-locked <silmatriel@gmail.com>
2021-12-23 04:10:47 +0300

    Init

--- M-Audio Axiom AIR 32 MINI/device_AixomAIR32.py
+++ M-Audio Axiom AIR 32 MINI/device_AixomAIR32.py
@@ -3,46 +3,46 @@
 import midi
 import transport
 import channels
+import device
 
 Buttons = {
     'Start':  0x75,
     'Stop': 0x74,
-    'ChannelUp': 0x2b,
-    'ChannelDown': 0x2a,
-    'ChannelSelector': 0x16,
+    'Rec': 0x76,
+    'ChannelUp': 0x14,
+    'ChannelDown': 0x13,
 }
 
-def On
-
 def OnMidiMsg(event):
-#    print(hex(event.data1), event.data2, event.midiId)  #debug
-    if event.midiId == midi.MIDI_CONTROLCHANGE:
+    print(f'data1: {hex(event.data1)},\ndata2: {event.data2},\nchannel:{event.midiChan},\nport:{event.port},\nmidiId: {event.midiId}\n')  #debug
 
-        #Transport buttons
-        #Stop
-        if (event.data1 == Buttons['Stop'] and event.data2 > 0):
-#            transport.stop()
-            transport.globalTransport(midi.FPT_Stop, 1)
-            event.handled = True
+def OnControlChange(event):
 
-        #Start
-        elif (event.data1 == Buttons['Start'] and event.data2 > 0):
-            transport.start()
-            event.handled = True
+    #Transport buttons
+    #Start
+    if (event.data1 == Buttons['Start'] and event.data2 > 0):
+        transport.start()
+        event.handled = True
+    #Stop
+    elif (event.data1 == Buttons['Stop'] and event.data2 > 0):
+        transport.stop()
+#        transport.globalTransport(midi.FPT_Stop, 1)
+        event.handled = True
+    #Rec
+    elif(event.data1 == Buttons['Rec'] and event.data2 > 0):
+        transport.record()
+        event.handled = True
 
-        #Channel up
-        elif ((event.data1 == Buttons['ChannelSelector'] and event.data2 > 0x40) or (event.data1 == Buttons['ChannelUp'] and event.data2 > 0)):
-            if channels.selectedChannel() < channels.channelCount()-1:
-                channels.selectOneChannel(channels.selectedChannel(1)+1)
-            event.handled = True
 
-        #Channel down
-        elif ((event.data1 == Buttons['ChannelSelector'] and event.data2 < 0x40) or (event.data1 == Buttons['ChannelDown'] and event.data2 > 0)):
-            if channels.selectedChannel() > 0:
-                channels.selectOneChannel(channels.selectedChannel()-1)
-            event.handled = True
+    #Channel up
+    elif (event.data1 == Buttons['ChannelUp'] and event.data2 > 0):
+        if channels.selectedChannel() < channels.channelCount()-1:
+            channels.selectOneChannel(channels.selectedChannel(1)+1)
+        event.handled = True
 
-    # If the script does not recognize the event, do nothing.
-    # It's then passed onto FL Studio to use.
-        else:
-            event.handled = False
+    #Channel down
+    elif (event.data1 == Buttons['ChannelDown'] and event.data2 > 0):
+        if channels.selectedChannel() > 0:
+            channels.selectOneChannel(channels.selectedChannel()-1)
+        event.handled = True
+    
