53fd1b2d61573087cd335ada769d2f2f42f566d2
giant-locked <silmatriel@gmail.com>
2021-12-23 06:29:23 +0300

    air32 updates

--- M-Audio Axiom AIR 32 MINI/device_AixomAIR32.py
+++ M-Audio Axiom AIR 32 MINI/device_AixomAIR32.py
@@ -1,48 +1,77 @@
 # name=M-Audio Axiom AIR 32 MINI
 
 import midi
+import ui
 import transport
 import channels
+import mixer
 import device
+from time import sleep
 
 Buttons = {
-    'Start':  0x75,
-    'Stop': 0x74,
-    'Rec': 0x76,
-    'ChannelUp': 0x14,
-    'ChannelDown': 0x13,
+    'Stop': 0x10,
+    'Start':  0x11,
+    'Rec': 0x12,
+    'Up': 0x13,
+    'Down': 0x14,
+    'Right': 0x15,
+    'Left': 0x16,
+    'Middle': 0x17,
+    'Hyper': 0x3A,
 }
+
+#def OnMidiIn(event):
+#    event.handled = False
+#    if event.data1 == Buttons['Hyper']:
+#        if event.data2 > 0:
+#            ui.setHintMsg(SECONDAY_MODE_HINT)
+#            SEC_COUNTER = 2
+#        else:
+#            ui.setHintMsg('')
+
+#def OnInit():
+#    print(mixer.trackNumber())
 
 def OnMidiMsg(event):
     print(f'data1: {hex(event.data1)},\ndata2: {event.data2},\nchannel:{event.midiChan},\nport:{event.port},\nmidiId: {event.midiId}\n')  #debug
 
 def OnControlChange(event):
+    if event.data2 > 0:
 
-    #Transport buttons
-    #Start
-    if (event.data1 == Buttons['Start'] and event.data2 > 0):
-        transport.start()
-        event.handled = True
-    #Stop
-    elif (event.data1 == Buttons['Stop'] and event.data2 > 0):
-        transport.stop()
-#        transport.globalTransport(midi.FPT_Stop, 1)
-        event.handled = True
-    #Rec
-    elif(event.data1 == Buttons['Rec'] and event.data2 > 0):
-        transport.record()
-        event.handled = True
+        #Transport buttons
+        #Start
+        if event.data1 == Buttons['Start']:
+            transport.start()
+            event.handled = True
+        #Stop
+        elif event.data1 == Buttons['Stop']:
+            transport.stop()
+    #        transport.globalTransport(midi.FPT_Stop, 1)
+            event.handled = True
+        #Rec
+        elif event.data1 == Buttons['Rec']:
+            transport.record()
+            event.handled = True
 
+        #Channel navigation
+        #Channel up
+        elif (event.data1 == Buttons['Down'] and channels.selectedChannel() < channels.channelCount() - 1):
+            channels.selectOneChannel(channels.selectedChannel(1) + 1)
+            event.handled = True
 
-    #Channel up
-    elif (event.data1 == Buttons['ChannelUp'] and event.data2 > 0):
-        if channels.selectedChannel() < channels.channelCount()-1:
-            channels.selectOneChannel(channels.selectedChannel(1)+1)
-        event.handled = True
+        #Channel down
+        elif (event.data1 == Buttons['Up'] and channels.selectedChannel() > 0):
+            channels.selectOneChannel(channels.selectedChannel() - 1)
+            event.handled = True
 
-    #Channel down
-    elif (event.data1 == Buttons['ChannelDown'] and event.data2 > 0):
-        if channels.selectedChannel() > 0:
-            channels.selectOneChannel(channels.selectedChannel()-1)
-        event.handled = True
-    
+        #Mixer navigation
+        #Right
+        elif (event.data1 == Buttons['Right'] ):
+            mixer.setTrackNumber(mixer.trackNumber() + 1, midi.curfxScrollToMakeVisible)
+
+        #Left
+        elif (event.data1 == Buttons['Left'] ):
+            mixer.setTrackNumber(mixer.trackNumber() - 1, midi.curfxScrollToMakeVisible)
+#            while event.data2 > 0:
+#                sleep(1)
+#                mixer.setTrackNumber(mixer.trackNumber() - 1, midi.curfxScrollToMakeVisible)
